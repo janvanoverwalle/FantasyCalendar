e51ef86399954064e6b76dc700536ef45ff65216
Jan Van Overwalle <janvanoverwalle@gmail.com>
2017-09-27 08:20:38 +0200

    Added initial 'Calendar' model.

--- app/src/main/java/com/jvanoverwalle/fantasycalendar/MainActivity.java
+++ app/src/main/java/com/jvanoverwalle/fantasycalendar/MainActivity.java
@@ -2,12 +2,20 @@ package com.jvanoverwalle.fantasycalendar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.jvanoverwalle.fantasycalendar.models.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getCanonicalName() + "-debug";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Calendar c = Calendar.loadFromJson("{\"year_len\":364,\"events\":1,\"n_months\":12,\"months\":[\"Morning Star\",\"Sun's Dawn\",\"First Seed\",\"Rain's Hand\",\"Second Seed\",\"Mid Year\",\"Sun's Height\",\"Last Seed\",\"Hearthfire\",\"Frost Fall\",\"Sun's Dusk\",\"Evening Star\"],\"month_len\":{\"Morning Star\":30,\"Sun's Dawn\":30,\"First Seed\":31,\"Rain's Hand\":30,\"Second Seed\":30,\"Mid Year\":31,\"Sun's Height\":30,\"Last Seed\":30,\"Hearthfire\":31,\"Frost Fall\":30,\"Sun's Dusk\":30,\"Evening Star\":31},\"week_len\":7,\"weekdays\":[\"Morndas\",\"Tirdas\",\"Middas\",\"Turdas\",\"Fredas\",\"Loredas\",\"Sundas\"],\"n_moons\":2,\"moons\":[\"Masser\",\"Secunda\"],\"lunar_cyc\":{\"Masser\":12,\"Secunda\":25},\"lunar_shf\":{\"Masser\":0,\"Secunda\":0},\"year\":2026,\"first_day\":0,\"notes\":{\"2026-1-1\":\"Test\"}}");
+
+        Log.d(LOG_TAG, c.toString());
     }
 }
---
+++ app/src/main/java/com/jvanoverwalle/fantasycalendar/models/Calendar.java
@@ -0,0 +1,319 @@
+package com.jvanoverwalle.fantasycalendar.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Jan Van Overwalle on 26/09/2017.
+ */
+
+public class Calendar {
+    //region Example Json Data
+    /*
+    {
+       "yearlen":364,
+       "events":1,
+       "nmonths":12,
+       "months":[
+          "Morning Star",
+          "Sun's Dawn",
+          "First Seed",
+          "Rain's Hand",
+          "Second Seed",
+          "Mid Year",
+          "Sun's Height",
+          "Last Seed",
+          "Hearthfire",
+          "Frost Fall",
+          "Sun's Dusk",
+          "Evening Star"
+       ],
+       "monthlen":{
+          "Morning Star":30,
+          "Sun's Dawn":30,
+          "First Seed":31,
+          "Rain's Hand":30,
+          "Second Seed":30,
+          "Mid Year":31,
+          "Sun's Height":30,
+          "Last Seed":30,
+          "Hearthfire":31,
+          "Frost Fall":30,
+          "Sun's Dusk":30,
+          "Evening Star":31
+       },
+       "weeklen":7,
+       "weekdays":[
+          "Morndas",
+          "Tirdas",
+          "Middas",
+          "Turdas",
+          "Fredas",
+          "Loredas",
+          "Sundas"
+       ],
+       "nmoons":2,
+       "moons":[
+          "Masser",
+          "Secunda"
+       ],
+       "lunarcyc":{
+          "Masser":12,
+          "Secunda":25
+       },
+       "lunarshf":{
+          "Masser":0,
+          "Secunda":0
+       },
+       "year":2026,
+       "firstday":0,
+       "notes":{
+          "2026-1-1":"Test"
+       }
+    }
+     */
+    //endregion
+
+    private String name;
+
+    private int yearLength;
+    private boolean events;
+
+    private int numMonths;
+    private String[] months;
+    private Map<String, Integer> monthLengths;
+
+    private int weekLength;
+    private String[] weekDays;
+
+    private int numMoons;
+    private String[] moons;
+
+    private Map<String, Float> lunarCycles;
+    private Map<String, Integer> lunarShifts;
+
+    private int year;
+    private int firstDay;
+
+    private Map<String, String> notes;
+
+    public Calendar() {
+        name = "Earth";
+
+        yearLength = 365;
+        events = true;
+
+        numMonths = 12;
+        months = new String[numMonths];
+        months[0] = "January";
+        months[1] = "February";
+        months[2] = "March";
+        months[3] = "April";
+        months[4] = "May";
+        months[5] = "June";
+        months[6] = "July";
+        months[7] = "August";
+        months[8] = "September";
+        months[9] = "October";
+        months[10] = "November";
+        months[11] = "December";
+        monthLengths = new HashMap<String, Integer>();
+        monthLengths.put("January",31);
+        monthLengths.put("February",28);
+        monthLengths.put("March",31);
+        monthLengths.put("April",30);
+        monthLengths.put("May",31);
+        monthLengths.put("June",30);
+        monthLengths.put("July",31);
+        monthLengths.put("August",31);
+        monthLengths.put("September",30);
+        monthLengths.put("October",31);
+        monthLengths.put("November",30);
+        monthLengths.put("December",31);
+
+        weekLength = 7;
+        weekDays = new String[weekLength];
+        weekDays[0] = "Sunday";
+        weekDays[1] = "Monday";
+        weekDays[2] = "Tuesday";
+        weekDays[3] = "Wednesday";
+        weekDays[4] = "Thursday";
+        weekDays[5] = "Friday";
+        weekDays[6] = "Saturday";
+
+        numMoons = 1;
+        moons = new String[numMoons];
+        moons[0] = "Luna";
+
+        lunarCycles = new HashMap<String, Float>();
+        lunarCycles.put("Luna", 29.53f);
+        lunarShifts = new HashMap<String, Integer>();
+        lunarShifts.put("Luna", 20);
+
+        year = 2017;
+        firstDay = 0;
+
+        notes = new HashMap<String, String>();
+    }
+
+    //region Getters & Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYearLength() {
+        return yearLength;
+    }
+
+    public void setYearLength(int yearLength) {
+        this.yearLength = yearLength;
+    }
+
+    public boolean hasEvents() {
+        return events;
+    }
+
+    public void setEvents(boolean events) {
+        this.events = events;
+    }
+
+    public int getNumMonths() {
+        return numMonths;
+    }
+
+    public void setNumMonths(int numMonths) {
+        this.numMonths = numMonths;
+    }
+
+    public String[] getMonths() {
+        return months;
+    }
+
+    public void setMonths(String[] months) {
+        this.months = months;
+    }
+
+    public Map<String, Integer> getMonthLengths() {
+        return monthLengths;
+    }
+
+    public void setMonthLengths(Map<String, Integer> monthLengths) {
+        this.monthLengths = monthLengths;
+    }
+
+    public int getWeekLength() {
+        return weekLength;
+    }
+
+    public void setWeekLength(int weekLength) {
+        this.weekLength = weekLength;
+    }
+
+    public String[] getWeekDays() {
+        return weekDays;
+    }
+
+    public void setWeekDays(String[] weekDays) {
+        this.weekDays = weekDays;
+    }
+
+    public int getNumMoons() {
+        return numMoons;
+    }
+
+    public void setNumMoons(int numMoons) {
+        this.numMoons = numMoons;
+    }
+
+    public String[] getMoons() {
+        return moons;
+    }
+
+    public void setMoons(String[] moons) {
+        this.moons = moons;
+    }
+
+    public Map<String, Float> getLunarCycles() {
+        return lunarCycles;
+    }
+
+    public void setLunarCycles(Map<String, Float> lunarCycles) {
+        this.lunarCycles = lunarCycles;
+    }
+
+    public Map<String, Integer> getLunarShifts() {
+        return lunarShifts;
+    }
+
+    public void setLunarShifts(Map<String, Integer> lunarShifts) {
+        this.lunarShifts = lunarShifts;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getFirstDay() {
+        return firstDay;
+    }
+
+    public void setFirstDay(int firstDay) {
+        this.firstDay = firstDay;
+    }
+
+    public Map<String, String> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(Map<String, String> notes) {
+        this.notes = notes;
+    }
+    //endregion
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public static Calendar loadFromJson(String jsonData) {
+        Calendar c = new Calendar();
+
+        try {
+            JSONObject jsonObject = new JSONObject(jsonData);
+
+            c.setYearLength(jsonObject.getInt("year_len"));
+            c.setEvents(jsonObject.getBoolean("events"));
+
+            c.setNumMonths(jsonObject.getInt("n_months"));
+            JSONArray jsonMonths = jsonObject.getJSONArray("months");
+            JSONArray jsonMonthLengths = jsonObject.getJSONArray("month_len");
+
+            String[] months = new String[c.getNumMonths()];
+            Map<String, Integer> monthLengths = new HashMap<String, Integer>();
+
+            for (int i = 0; i < jsonMonths.length(); ++i) {
+                months[i] = jsonMonths.get(i).toString();
+                int monthLength = jsonMonthLengths.getJSONObject(i).getInt(months[i]);
+                monthLengths.put(months[i], monthLength);
+            }
+            c.setMonths(months);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return c;
+    }
+}
